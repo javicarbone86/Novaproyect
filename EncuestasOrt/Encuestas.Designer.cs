efdb0b4f84ff8b23e75f6c6cc0f2b79635936209
Shark <Shark@DESKTOP-PRSDB4A>
2016-10-08 19:04:37 -0300

    subo esta version que ya no rompe

--- EncuestasOrt/Encuestas.Designer.cs
+++ EncuestasOrt/Encuestas.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\posix\Documents\portal-encuestas-ort\EncuestasOrt\EncuestasOrt\Encuestas.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Shark\Source\Repos\Proyecto\Novaproyect\EncuestasOrt\Encuestas.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
